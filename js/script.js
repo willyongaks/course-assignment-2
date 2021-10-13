cd48524fbc7ee676877d5b2df8ad9f46582c9933
willyongaks <william.ongaki10@gmail.com>
2021-10-13 16:32:08 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+//question 1
 
+//Create a variable called outOfStock and assign it a boolean value.
+//Create an if else statement that checks the value of outOfStock.
+//If it is true, console log "Out of stock". Otherwise log "In stock".
+
+
+//answer 1
+var outOfStock = true;
+
+if (outOfStock ===true){
+    console.log("in stock")
+}
+else {
+    console.log("Out of stock")
+}
+
+
+//question 2
+
+//Create a for loop that counts from 15 to 25
+//Console log the value of the counter variable inside the loop only if it's equal to 17 or 20.
+
+
+for (var  i = 15; i<=25; i++) {
+    console.log()
+}
